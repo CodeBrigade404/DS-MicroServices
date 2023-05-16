3c8d6bc6972db917f759aed1882b0cf1eb5e4e32
ManulaK <it21019968@my.sliit.lk>
2023-05-16 08:04:07 +0530

    clear up

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       <button onClick={handleLogout}>Logout</button>
-      <ProductList></ProductList>
+      <ProductList />
     </div>
   );
 };
