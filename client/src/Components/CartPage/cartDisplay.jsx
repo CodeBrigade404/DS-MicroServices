fedbee1e5ad7489d9a9506d8265b8a738155386b
ManulaK <it21019968@my.sliit.lk>
2023-05-19 22:01:12 +0530

    feature/checkout

--- client/src/Components/CartPage/cartDisplay.jsx
+++ client/src/Components/CartPage/cartDisplay.jsx
@@ -114,19 +114,11 @@ function Cart() {
 
   const onClick = async (event) => {
     event.preventDefault();
-    const items = [
-      { id: 1, quantity: 3 },
-      { id: 2, quantity: 2 },
-      { id: 3, quantity: 2 },
-    ];
 
     try {
-      const response = await axios.post(
-        "http://localhost:4000/create-checkout-session",
-        {
-          items: items,
-        }
-      );
+      const response = await axios.post("http://localhost:4000/create-checkout-session", {
+        cartItems: cartItems,
+      });
       window.location = response.data.url;
     } catch (err) {
       console.log(err.message);
